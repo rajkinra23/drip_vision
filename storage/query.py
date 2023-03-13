42d2d185e9de1ca7f0774aa1b346f73e99bc28a4
Rajeev Kinra <rajkinra23@gmail.com>
2023-03-13 14:05:35 -0700

    Finish up storage/query notebook, create initial v0 storage/query library (still need to add tests), add some more test data.

---
+++ storage/query.py
@@ -0,0 +1,24 @@
+from redis import Redis
+from redis.commands.search.query import Query
+
+"""Interface to query similar images against indexed vectors in the redis db.
+"""
+
+class QueryEngine():
+    def __init__(self):
+        self.r = Redis(host = 'localhost', port = 6379)
+        self.ef_runtime = 10
+        self.product_image_vector_field = 'product_image_vector'
+        self.item_field_name = "item_name"
+        self.dim = 512
+
+
+    def query(self, query_vector, count):
+        # Build query
+        q = Query(f'*=>[KNN {topK} @{self.product_image_vector_field} $vec_param EF_RUNTIME {self.ef_runtime} AS vector_score]').sort_by('vector_score').paging(0, count).return_fields('vector_score','item_name').dialect(2)
+        params_dict = {"vec_param": query_vector}
+
+        # Run Query and return documents
+        results = self.r.ft().search(q, query_params = params_dict)
+        docs = self.r.ft().search(q, params_dict).docs
+        return docs
